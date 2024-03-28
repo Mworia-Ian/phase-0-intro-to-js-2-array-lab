b21bf4379c453e7b5f2be7c2af6b7cced2e1c831
Ian Mworia <ianmworia156@gmail.com>
2024-03-28 12:42:59 +0300

    Lab Comlete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+  cats.unshift("Bob");
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function appendCat() {
+  let newArray = [].concat(cats, "Broom");
+  return newArray;
+}
+
+function prependCat() {
+  let newCat = ["Arnold"].concat(cats);
+  return newCat;
+}
+
+function removeLastCat() {
+  removeLastCat = cats.slice(0, -1);
+  return removeLastCat
+}
+
+function removeFirstCat(){
+    // removeFirstCat = cats.slice(0, 0)
+    // return removeFirstCat
+    let [, ...removeFirstCat] = cats;
+    return removeFirstCat
+}
